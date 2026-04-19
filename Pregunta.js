5613380d238e03a3816be21872472e4344b2ffc7
maleja1056 <a.fajardo1056@gmail.com>
2026-04-19 10:38:43 -0500

    Pregunta

---
+++ Pregunta.js
@@ -0,0 +1,17 @@
+{
+    "_id": 1,
+    "producto_id": 1,
+    "pregunta": "¿Trae tarjeta gráfica dedicada?",
+    "fecha": "2024-01-16",
+    "anonimo": false,
+    "cliente_id": ,
+    "usuario_id": 1,
+    "respuestas": [
+        {
+            "respuesta": "Sí, trae NVIDIA RTX 3060",
+            "fecha": "2024-01-17",
+            "vendedor_id":1
+        }
+    ]
+
+}
