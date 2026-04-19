858f66304f5588dbef11e58777cfc1dc06a30be8
maleja1056 <a.fajardo1056@gmail.com>
2026-04-19 10:37:47 -0500

    valoracion

---
+++ Valoracion.js
@@ -0,0 +1,11 @@
+{
+    "_id":1 ,
+    "producto_id":1 ,
+    "estrellas": 5,
+    "cliente_id":,
+    "comentario": "Excelente producto",
+    "fecha": "2024-01-18",
+    "anonimo": false,
+    "usuario_id":1
+
+}
