078de8844a56bf5718ce6b64dd72445a32fe9316
maleja1056 <a.fajardo1056@gmail.com>
2026-04-19 10:36:15 -0500

    Clientes

---
+++ Comprador\vendedor.js
@@ -0,0 +1,20 @@
+{
+    "_id":1,
+    "nombre": "Juan Perez",
+    "documento": "12345678",
+    "fecha_nacimiento": "1990-05-15",
+    "direccion": {
+        "departamento": "Cundinamarca",
+        "ciudad": "Bogotá",
+        "direccion": "Calle 123 #45-67"
+    },
+    "correo": "juan@gmail.com",
+    "telefono": "3001234567",
+    "redes_sociales": {
+        "twitter": "@juanperez",
+        "facebook": "juanperez",
+        "instagram": "juanperez"
+    },
+    "tipo": "vendedor"
+
+}
